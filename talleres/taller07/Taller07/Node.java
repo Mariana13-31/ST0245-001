816b167bd2170d0601841f71ffbbc830e89a1644
isabellasernaa <68722534+isabellasernaa@users.noreply.github.com>
2020-09-11 20:56:58 -0500

    Add files via upload

---
+++ talleres/taller07/Taller07/Node.java
@@ -0,0 +1,18 @@
+
+/**
+ * 
+ * 
+ * @author Mauricio Toro
+ * @version 1
+ */
+// Un nodo para una lista simplemente enlazada
+public class Node {
+    public int data;
+    public Node next;
+    public Node(int data)
+    {
+        next = null;
+        this.data = data;
+    }
+    
+}
